14b4a0b7065f7e2145507fe45e02eb13a5c64d03
honestit <michal.kupisinski@gmail.com>
2020-11-21 17:25:04 +0100

    promises-maker :: added maker properties
    Properties for exchange name and queue name (AMQP) are set by properties and read by MakerProperties class

---
+++ maker/src/main/java/pl/honestit/projects/promises/maker/properties/MakerProperties.java
@@ -0,0 +1,14 @@
+package pl.honestit.projects.promises.maker.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("promises.services.maker")
+@Data
+public class MakerProperties {
+
+    private String exchangeName;
+    private String queueName;
+}
