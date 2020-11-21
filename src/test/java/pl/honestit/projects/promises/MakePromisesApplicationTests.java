e0513924fc6dc7314f74e786d8d176280c4f83a8
Honest IT <michal.kupisinski@gmail.com>
2020-11-21 06:08:35 +0100

    Initial project struture

---
+++ src/test/java/pl/honestit/projects/promises/MakePromisesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.honestit.projects.promises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakePromisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
