ead4f6615aaa8d9ec1a4aa0fdb711b3cd7886f71
honestit <michal.kupisinski@gmail.com>
2020-11-21 18:21:15 +0100

    promises-maker :: happy path for making promises
    Implementation of happy path for making promises with entity, converter and service. Service is invoked by message to configured queue.

---
+++ maker/src/main/java/pl/honestit/projects/promises/maker/converter/PromiseEntityConverter.java
@@ -0,0 +1,30 @@
+package pl.honestit.projects.promises.maker.converter;
+
+import org.springframework.stereotype.Component;
+import pl.honestit.projects.promises.maker.model.PromiseEntity;
+import pl.honestit.projects.promises.model.friend.FriendIdentity;
+import pl.honestit.projects.promises.model.promise.Deadline;
+import pl.honestit.projects.promises.model.promise.Promise;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+@Component
+public class PromiseEntityConverter {
+
+    public PromiseEntity from(Promise promise) {
+        return PromiseEntity.builder()
+                .who(promise.getWho())
+                .what(promise.getWhat())
+                .whom(Optional.ofNullable(promise.getWhom().getIdentity())
+                        .map(FriendIdentity::getUsername)
+                        .orElse(promise.getWhom().getName()))
+                .when(promise.getWhen())
+                .till(asZoneDateTime(promise.getTill()))
+                .build();
+    }
+
+    private ZonedDateTime asZoneDateTime(Deadline deadline) {
+        return ZonedDateTime.of(deadline.getTillDate().atTime(deadline.getTillTime()), deadline.getZoneId());
+    }
+}
--- maker/src/main/java/pl/honestit/projects/promises/maker/listener/PromiseListener.java
+++ maker/src/main/java/pl/honestit/projects/promises/maker/listener/PromiseListener.java
@@ -3,6 +3,8 @@ package pl.honestit.projects.promises.maker.listener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.honestit.projects.promises.maker.executor.PromiseMaker;
 import pl.honestit.projects.promises.model.promise.Promise;
 
 @Service
@@ -10,7 +12,19 @@ import pl.honestit.projects.promises.model.promise.Promise;
 @RequiredArgsConstructor
 public class PromiseListener {
 
+    private final PromiseMaker promiseMaker;
+
+    @Transactional
     public void receivedPromise(Promise promise) {
         log.debug("Received promise: {}", promise);
+
+        boolean made = promiseMaker.make(promise);
+        if (made) {
+            log.debug("Promise was successfully made");
+        } else {
+            log.debug("Promise somehow was not made");
+            log.debug("Sending promise to discarded queue");
+        }
+
     }
 }
